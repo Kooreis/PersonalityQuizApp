7992dadc2154b6dda0b022ed014bd9b1235a665f
Kooreis <kooreis.official@gmail.com>
2022-12-29 03:00:00 -0600

    In this update, the basic structure of the program is set up, including the namespace, class, and main method. An array of questions for the personality quiz is also defined.

--- Personality_Quiz_App.cs
+++ Personality_Quiz_App.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 namespace PersonalityQuizApp
@@ -13,38 +12,6 @@ namespace PersonalityQuizApp
                 "Do you prefer reading books over watching movies? (yes/no)",
                 "Do you consider yourself an introvert? (yes/no)"
             };
-
-            string[] answers = new string[questions.Length];
-
-            for (int i = 0; i < questions.Length; i++)
-            {
-                Console.WriteLine(questions[i]);
-                answers[i] = Console.ReadLine();
-            }
-
-            int score = 0;
-
-            foreach (string answer in answers)
-            {
-                if (answer.ToLower() == "yes")
-                {
-                    score++;
-                }
-            }
-
-            if (score <= 1)
-            {
-                Console.WriteLine("Your personality type is: Type A");
-            }
-            else if (score == 2)
-            {
-                Console.WriteLine("Your personality type is: Type B");
-            }
-            else
-            {
-                Console.WriteLine("Your personality type is: Type C");
-            }
         }
     }
 }
-```
